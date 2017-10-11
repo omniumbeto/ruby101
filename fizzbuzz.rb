15d5c2a420f6b66f3463519c1f5f7c5bd4b1819c
omniumbeto <proyectos@omniumcorp.com>
2017-10-11 17:58:46 -0500

    added first attempt of fizzbuzz

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,12 +1,9 @@
-puts'Lets play fizzbuzz'
-print'enter a number: '
-number = gets.chomp.to_i
-puts "this is your number: #{number}"
+  puts'Lets play fizzbuzz'
+  print'enter a number: '
+  number = gets.chomp.to_i
+  puts "this is your number: #{number}"
 if number % 3 == 0
    puts "Fizz buddy!"
-
 elsif number % 5 == 0
    puts "Buzz buddy!"
-else
-    puts "stupid donkey!"
 end
