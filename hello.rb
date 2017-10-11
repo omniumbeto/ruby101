2539b57174b6ec0541fa6033dc73630c4795dda1
omniumbeto <proyectos@omniumcorp.com>
2017-10-10 20:12:47 -0500

    add hello.rb

---
+++ hello.rb
@@ -0,0 +1,45 @@
+#
+# name = "Beto" #cambialo por tu nobmre
+# age=35
+# puts name
+# puts age
+#
+#
+# puts "Enter your name"
+# name = gets.chomp
+# print name
+# puts ", hola."
+
+
+
+#escribir programa que pida al usuario su nombre y su edad y que imprima la cadena "Hola [nombre], naciste en el año [año]."
+# puts " Enter your name"
+# name = gets.chomp.to_i
+# print name
+# puts ", please enter your age"
+# age = gets.chomp.to_i
+# puts "Hi #{name}, you were born in #{2017-age}."
+
+# puts " Enter your name"
+# name = gets.chomp.to_i
+# print name
+# puts ", please enter your age"
+# age = gets.chomp.to_i
+# year=Time.new.year - age
+# puts "Hi #{name}, you were born in #{year}."
+
+
+# puts " Enter your name"
+# name = gets.chomp.to_i
+# print name
+# puts ", please enter your age"
+# age = gets.chomp.to_i
+# year=Time.new.year - age
+# puts "Hi #{name}, you were born in #{year}."
+
+# print " Enter your name"
+# name = gets.chomp
+# print "please enter your age"
+# age = gets.chomp.to_i
+# year=Time.new.year - age
+# puts "Hi #{name"}, you were born in #{year}."
